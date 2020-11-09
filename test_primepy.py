9718ee1a6c552a8159e7bba4e9e6c3932503382d
Andrea <gennari.andrea.98@gmail.com>
2020-11-09 10:54:53 +0100

    test function is_prime

---
+++ test_primepy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 10:52:51 2020
+
+@author: Andrea
+"""
+
+
+from function_fraction import is_prime
+
+def test_prime():
+        assert is_prime(7) == 1
